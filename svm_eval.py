ae575030f01bb6ef0444df0bd9ca7e772dbc0370
Adithya <as20130@nyu.edu>
2026-04-30 01:34:09 -0400

    removed extra files and fixed an error

--- svm_eval.py
+++
@@ -1,9 +0,0 @@
-from dataset import load_data
-from svm import SVM_with_Kernel
-X, y, feature_names = load_data("heart.csv")
-feature_names = [f.strip() for f in feature_names]
-
-accuracy, precision, recall = SVM_with_Kernel(X, y)
-print(f"Accuracy:  {accuracy * 100:.2f}%")
-print(f"Precision: {precision * 100:.2f}%")
-print(f"Recall:    {recall * 100:.2f}%")
